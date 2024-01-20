295c52f75e55cedb13149ae67575ea552d7554bc
Artem <atremat@gmail.com>
2024-01-21 00:12:29 +0200

    izitoast via cdn added

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -22,7 +22,6 @@ const options = {
     userSelectedDate = selectedDates[0].getTime();
     //enable button
     refs.startBtn.disabled = false;
-    //Якщо користувач вибрав валідну дату (в майбутньому), кнопка «Start» стає активною.
   },
 };
 
@@ -34,8 +33,40 @@ const refs = {
 //store the instance of flatpickr
 const fp = flatpickr(refs.dateTime, options);
 
+//start button listener
 refs.startBtn.addEventListener('click', onBtnClick);
 
+//callback of start button listener
 function onBtnClick(e) {
   console.log(e);
 }
+
+//converts ms to the object { days, hours, minutes, seconds }
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+//adds zero before single digit numbers
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+iziToast.show({
+  title: 'Hey',
+  message: 'What would you like to add?',
+});
